cdbb3dc977164cbc0a646507738f5651fdbe351c
Elijah <ejoleg@gmail.com>
2015-06-17 09:21:32 -0700

    Honoi

---
+++ towers/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var thePlate;
+	var thePlane;
+	$(".plate").click(function(e) {
+    e.stopPropagation();
+		if ($(this).is(':first-child')) {
+			thePlate = this
+		}
+		console.log(thePlate);
+	});
+
+	$(".plane").click(function() {
+		thePlane = this;
+		console.log(thePlane);
+		if (thePlate) {
+			if ($(this).children().length) {
+				console.log("this is the plate", $(thePlate).text());
+				console.log("this is the firstchild", $(this).find(":first-child").text());
+				$(thePlate).text() < ($(this).find(":first-child").text()) ? $(thePlane).append(thePlate) : alert("Dont stack bigger plates on smaller plates");
+			} else {
+				$(thePlane).append(thePlate)
+			}
+			thePlate = [];
+			thePlane = [];
+		}
+		if ($("#last-plane").children().length === 3) {
+			alert("you won, bro");
+		}
+	});
+});
